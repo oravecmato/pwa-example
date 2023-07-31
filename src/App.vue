6714bb23f2b20c59dd565616c087995273054ab0
oravecmato <oravec.mato@gmail.com>
2023-07-31 02:09:34 +0200

    move sw installation into the index.html

--- src/App.vue
+++ src/App.vue
@@ -7,79 +7,77 @@
 import PDFReader from './components/PDFReader.vue'
 import HelloWorld from './components/HelloWorld.vue';
 import {onMounted, ref} from "vue";
-import { register } from 'register-service-worker';
+// import { register } from 'register-service-worker';
 
-const blockReader = ref(true);
+const blockReader = ref(false);
 onMounted(() => {
-    register(`${import.meta.env.BASE_URL}sw.js`, {
-      ready(registration) {
-        console.log('App is being served from cache by a service worker');
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     register(`${import.meta.env.BASE_URL}sw.js`, {
+//       ready(registration) {
+//         console.log('App is being served from cache by a service worker');
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//
 
-        setTimeout(() => {
-          blockReader.value = false;
-        }, 3000);
-
-        registration.active.postMessage({
-          type: 'MESSAGE_IDENTIFIER',
-        });
-
-        const messageChannel = new MessageChannel();
-
-// First we initialize the channel by sending
-// the port to the Service Worker (this also
-// transfers the ownership of the port)
-        registration.active.postMessage({
-          type: 'INIT_PORT',
-        }, [messageChannel.port2]);
-
-// Listen to the response
-        messageChannel.port1.onmessage = (event) => {
-          // Print the result
-          console.log(event.data.payload);
-        };
-
-// Then we send our first message
-        registration.active.postMessage({
-          type: 'INCREASE_COUNT',
-        });
-
-        // Setup a listener to receive messages from the service worker
-        function listenForMessage(serviceWorker) {
-          if (serviceWorker) {
-            serviceWorker.addEventListener('message', event => {
-              console.log('Received a message from service worker: ', event.data);
-            });
-
-            // Send a message to the service worker
-            serviceWorker.postMessage('Are you there, SW?');
-          }
-        }
-
-        // Set up listeners on all possible states of the service worker
-        listenForMessage(registration.installing);
-        listenForMessage(registration.waiting);
-        listenForMessage(registration.active);
-      },
-      registered() {
-        console.log('Service worker has been registered')
-      },
-      cached() {
-        console.log('Content has been cached for offline use')
-      },
-      updatefound() {
-        console.log('New content is downloading')
-      },
-      updated() {
-        console.log('New content is available, please refresh')
-      },
-      offline() {
-        console.log('No internet conection found. App is running in offline mode.')
-      },
-      error(error) {
-        console.error('Error during service worker registration:', error)
-      },
-    })
+//
+//         registration.active.postMessage({
+//           type: 'MESSAGE_IDENTIFIER',
+//         });
+//
+//         const messageChannel = new MessageChannel();
+//
+// // First we initialize the channel by sending
+// // the port to the Service Worker (this also
+// // transfers the ownership of the port)
+//         registration.active.postMessage({
+//           type: 'INIT_PORT',
+//         }, [messageChannel.port2]);
+//
+// // Listen to the response
+//         messageChannel.port1.onmessage = (event) => {
+//           // Print the result
+//           console.log(event.data.payload);
+//         };
+//
+// // Then we send our first message
+//         registration.active.postMessage({
+//           type: 'INCREASE_COUNT',
+//         });
+//
+//         // Setup a listener to receive messages from the service worker
+//         function listenForMessage(serviceWorker) {
+//           if (serviceWorker) {
+//             serviceWorker.addEventListener('message', event => {
+//               console.log('Received a message from service worker: ', event.data);
+//             });
+//
+//             // Send a message to the service worker
+//             serviceWorker.postMessage('Are you there, SW?');
+//           }
+//         }
+//
+//         // Set up listeners on all possible states of the service worker
+//         listenForMessage(registration.installing);
+//         listenForMessage(registration.waiting);
+//         listenForMessage(registration.active);
+//       },
+//       registered() {
+//         console.log('Service worker has been registered')
+//       },
+//       cached() {
+//         console.log('Content has been cached for offline use')
+//       },
+//       updatefound() {
+//         console.log('New content is downloading')
+//       },
+//       updated() {
+//         console.log('New content is available, please refresh')
+//       },
+//       offline() {
+//         console.log('No internet conection found. App is running in offline mode.')
+//       },
+//       error(error) {
+//         console.error('Error during service worker registration:', error)
+//       },
+//     })
 
 });
 </script>
